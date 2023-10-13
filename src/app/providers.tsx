0c0814c28518a417ebc1fffc36875ccf6a90d886
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2023-10-13 09:51:29 -0400

    Quitando Codigo Inutilizado

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { UserProvider } from "./context/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      <UserProvider>{children}</UserProvider>
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
